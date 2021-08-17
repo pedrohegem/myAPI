7311d150ea774f4678fc7d458b9101005a4e74a9
pedrohegem <hegemcodes@gmail.com>
2021-08-17 10:24:24 +0200

    built

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 morgan.token('body', req => JSON.stringify(req.body))
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
